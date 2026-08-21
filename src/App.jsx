13d8486030a1154cb8b570d187f6499a81c2ae03
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-08-21 08:29:37 +0200

    Fixed tables and adjusted input fields

--- src/App.jsx
+++ src/App.jsx
@@ -12,54 +12,64 @@ function App() {
         <form>
 
           <table id="starting-amt-tbl"> 
-            <td width="250"><label>Starting Amount</label></td>
-            <td>
-              <span>R</span>
-              <input type="text"></input>
-            </td>
+            <tr>
+              <td width="250"><label>Starting Amount</label></td>
+              <td>
+                <input type="text" ></input>
+                <span class="inuirandspan">R</span>
+              </td>
+            </tr>
           </table>
 
-          <table id="final-amt-tbl">
-            <td width="220"><label>After</label></td>
-            <td>
-              <input type="text"></input>
-              <span>years</span>
-            </td>
+          <table id="amt-time-tbl">
+            <tr>
+              <td width="250"><label>After</label></td>
+              <td>
+                <input type="text"></input>
+                <span class="inuiyearspan">years</span>
+              </td>
+            </tr>
           </table>
 
           <table id="rate-of-return-tbl">
-            <td width="250"><label>Rate of Return</label></td>
-            <td>
-              <input type="text"></input>
-              <span>%</span>
-            </td>
+            <tr>
+              <td width="250"><label>Rate of Return</label></td>
+              <td>
+                <input type="text"></input>
+                <span class="inuipercentspan">%</span>
+              </td>
+            </tr>
           </table>
 
           <table id="compounding-period-tbl">
-            <td width="220"><label>Compounded</label></td>
-            <td>
-              <select name="compounding-period" id="compounding-period" class="custom-dropdown">
+            <tr>
+              <td width="250"><label>Compounded</label></td>
+              <td>
+                <select name="compounding-period" id="compounding-period" class="custom-dropdown">
 
-                <option value="continuously">continuously</option>
-                <option value="daily">daily</option>
-                <option value="weekly">weekly</option>
-                <option value="biweekly">biweekly</option>
-                <option value="semimonthly">semimonthly</option>
-                <option value="monthly">monthly</option>
-                <option value="quarterly">quarterly</option>
-                <option value="semiannually">semiannually</option>
-                <option value="annually">annually</option>
+                  <option value="continuously">continuously</option>
+                  <option value="daily">daily</option>
+                  <option value="weekly">weekly</option>
+                  <option value="biweekly">biweekly</option>
+                  <option value="semimonthly">semimonthly</option>
+                  <option value="monthly">monthly</option>
+                  <option value="quarterly">quarterly</option>
+                  <option value="semiannually">semiannually</option>
+                  <option value="annually">annually</option>
 
-              </select>
-            </td>
+                </select>
+              </td>
+            </tr>
           </table>
 
           <table id="add-contribution-tbl">
-            <td width="250"><label>Additional Contribution</label></td>
-            <td>
-              <span>R</span>
-              <input type="text"></input> 
-            </td>
+            <tr>
+              <td width="250"><label>Additional Contribution</label></td>
+              <td>
+                <input type="text"></input> 
+                <span class="inuirandspan">R</span>
+              </td>
+            </tr>
           </table>
 
           <section>
