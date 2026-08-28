233825dca4a2620830ccb1cbfcc1a0faa3e38cad
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-08-28 10:01:21 +0200

    Outputs section

--- src/App.jsx
+++ src/App.jsx
@@ -7,91 +7,132 @@ function App() {
   return (
     <>
 
-      <div id="calculator">
+      <table>
 
-        <form>
+        <tr>
 
-          <table id="starting-amt-tbl"> 
-            <tr>
-              <td width="250"><label>Starting Amount</label></td>
-              <td>
-                <input type="text" class="input-controls monetary-field"></input>
-                <span class="inuirandspan">R</span>
-              </td>
-            </tr>
-          </table>
+          <td>
 
-          <table id="amt-time-tbl">
-            <tr>
-              <td width="250"><label>After</label></td>
-              <td>
-                <input type="text" class="input-controls" maxLength={4}></input>
-                <span class="inuiyearspan">years</span>
-              </td>
-            </tr>
-          </table>
+            <div id="calculator">
 
-          <table id="rate-of-return-tbl">
-            <tr>
-              <td width="250"><label>Rate of Return</label></td>
-              <td>
-                <input type="text" class="input-controls" maxLength={10}></input>
-                <span class="inuipercentspan">%</span>
-              </td>
-            </tr>
-          </table>
+              <form>
 
-          <table id="compounding-period-tbl">
-            <tr>
-              <td width="250"><label>Compounded</label></td>
-              <td>
-                <select name="compounding-period" id="compounding-period" class="custom-dropdown input-controls">
+                <table id="starting-amt-tbl"> 
+                  <tr>
+                    <td width="250"><label>Starting Amount</label></td>
+                    <td>
+                      <input type="text" class="input-controls monetary-field"></input>
+                      <span class="inuirandspan">R</span>
+                    </td>
+                  </tr>
+                </table>
 
-                  <option value="continuously">continuously</option>
-                  <option value="daily">daily</option>
-                  <option value="weekly">weekly</option>
-                  <option value="biweekly">biweekly</option>
-                  <option value="semimonthly">semimonthly</option>
-                  <option value="monthly">monthly</option>
-                  <option value="quarterly">quarterly</option>
-                  <option value="semiannually">semiannually</option>
-                  <option value="annually">annually</option>
+                <table id="amt-time-tbl">
+                  <tr>
+                    <td width="250"><label>After</label></td>
+                    <td>
+                      <input type="text" class="input-controls" maxLength={4}></input>
+                      <span class="inuiyearspan">years</span>
+                    </td>
+                  </tr>
+                </table>
 
-                </select>
-              </td>
-            </tr>
-          </table>
+                <table id="rate-of-return-tbl">
+                  <tr>
+                    <td width="250"><label>Rate of Return</label></td>
+                    <td>
+                      <input type="text" class="input-controls" maxLength={9}></input>
+                      <span class="inuipercentspan">%</span>
+                    </td>
+                  </tr>
+                </table>
 
-          <table id="add-contribution-tbl">
-            <tr>
-              <td width="250"><label>Additional Contribution</label></td>
-              <td>
-                <input type="text" class="input-controls monetary-field"></input> 
-                <span class="inuirandspan">R</span>
-              </td>
-            </tr>
-          </table>
+                <table id="compounding-period-tbl">
+                  <tr>
+                    <td width="250"><label>Compounded</label></td>
+                    <td>
+                      <select name="compounding-period" id="compounding-period" class="custom-dropdown input-controls">
 
-          <section>
-            <label>Contribute at the</label>
-            <input type="radio"></input> <label>beginning</label>
-            <input type="radio"></input> <label>end</label>
+                        <option value="continuously">continuously</option>
+                        <option value="daily">daily</option>
+                        <option value="weekly">weekly</option>
+                        <option value="biweekly">biweekly</option>
+                        <option value="semimonthly">semimonthly</option>
+                        <option value="monthly">monthly</option>
+                        <option value="quarterly">quarterly</option>
+                        <option value="semiannually">semiannually</option>
+                        <option value="annually">annually</option>
 
-            <br/>
+                      </select>
+                    </td>
+                  </tr>
+                </table>
 
-            <label>of each</label>
-            <input type="radio"></input> <label>week</label>
-            <input type="radio"></input> <label>fortnight</label>
-            <input type="radio"></input> <label>month</label>
-            <input type="radio"></input> <label>year</label>
+                <table id="add-contribution-tbl">
+                  <tr>
+                    <td width="250"><label>Additional Contribution</label></td>
+                    <td>
+                      <input type="text" class="input-controls monetary-field"></input> 
+                      <span class="inuirandspan">R</span>
+                    </td>
+                  </tr>
+                </table>
+
+                <table id="contribution-timing">
+                  <tr>
+                    <td>
+
+                      <section>
+                        <label>Contribute at the</label>
+                        <input type="radio"></input> <label>beginning</label>
+                        <input type="radio"></input> <label>end</label>
+
+                        <br/>
+
+                        <label>of each</label>
+                        <input type="radio"></input> <label>week</label>
+                        <input type="radio"></input> <label>fortnight</label>
+                        <input type="radio"></input> <label>month</label>
+                        <input type="radio"></input> <label>year</label>
+                        
+                      </section>
+                      
+                    </td>
+                  </tr>
+                </table>
+
+              </form>
+
+            </div>
+
+          </td>
+
+          <td width="5%"></td>{/* spacing */}
+
+          <td>
+
+            <div id="outputs">
             
-          </section>
+              <h1 align="left">Results</h1>
 
-          <input type="button" value="Calculate"></input>
+              <table cellPadding={6} width="100%">
 
-        </form>
+                <tr>
 
-      </div>
+                  <td align="left"><b>Final Balance</b></td>
+                  <td align="right"><b>R000.00</b></td>
+
+                </tr>
+
+              </table>
+
+            </div>
+
+          </td>
+
+        </tr>
+        <input type="button" value="Calculate"></input>
+      </table>
 
     </>
   )
